83c4aa6ac067c7b7531ec1a4280ceb5d11c7a6e9
Daniel Toledo <dan992965@gmail.com>
2023-05-02 11:40:25 -0300

    feature: cria componente home e paper

---
+++ src/contexts/Props.tsx
@@ -0,0 +1,27 @@
+export type UserProps = {
+  name: string;
+  email: string;
+  balance: number;
+  transactions: {
+    id: number;
+    title: string;
+    type: string;
+    category: string;
+    amount: number;
+    date: Date;
+  }[];
+  cards: {
+    id: number;
+    title: string;
+    type: string;
+    limit: number;
+    balance: number;
+  }[];
+  goals: {
+    id: number;
+    title: string;
+    type: string;
+    amount: number;
+    balance: number;
+  }[];
+};
