fe57ab7b2aaff09836704f2cd2e2524d73241724
Daniel Toledo <dan992965@gmail.com>
2023-05-02 11:41:42 -0300

    feature: cria componente overallBalance que mostra o saldo no home

---
+++ src/components/Home/OverallBalance/OverallBalance.tsx
@@ -0,0 +1,47 @@
+import cn from 'classnames';
+
+import { useState } from 'react';
+
+import './OverallBalance.scss';
+
+import Paper from '../Paper/Paper';
+import PaperHeader from '../Paper/PaperHeader';
+
+type OverallBalanceProps = {
+  className?: string;
+  balance?: number;
+};
+
+const OverallBalance: React.FC<OverallBalanceProps> = ({ className, balance }) => {
+  const [balanceDate, setBalanceDate] = useState<Date>(new Date());
+
+  return (
+    <Paper>
+      <PaperHeader>
+        <h1>Saldo Geral</h1>
+        <button onClick={() => setBalanceDate(new Date())}>
+          <span className="material-symbols-outlined">update</span>
+        </button>
+      </PaperHeader>
+      <div className={cn('__overall-balance-content', className)}>
+        {balance?.toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        })}
+      </div>
+      <div className={cn('__overall-balance-footer')}>
+        <span>
+          Atualizado em{' '}
+          {balanceDate.toLocaleDateString('pt', {
+            month: '2-digit',
+            year: '2-digit',
+            day: '2-digit'
+          })}{' '}
+          às {balanceDate.getHours() + ':' + balanceDate.getMinutes()}
+        </span>
+      </div>
+    </Paper>
+  );
+};
+
+export default OverallBalance;
