a14ca62b7cd21e8347d7f43e3e84855c1c45e6f6
Daniel Toledo <dan992965@gmail.com>
2023-05-05 11:59:54 -0300

    fix: adiciona box-shadow padrao com o sass

--- src/components/Home/Paper/Paper.tsx
+++ src/components/Home/Paper/Paper.tsx
@@ -3,11 +3,12 @@ import cn from 'classnames';
 import './Paper.scss';
 
 type PaperProps = {
+  className?: string;
   children?: React.ReactNode;
 };
 
-const Paper: React.FC<PaperProps> = ({ children }) => {
-  return <div className={cn('__paper-container')}>{children}</div>;
+const Paper: React.FC<PaperProps> = ({ children, className }) => {
+  return <div className={cn('__paper-container', className)}>{children}</div>;
 };
 
 export default Paper;
