75b191235f89f42cb2225e0e11667255016bc84f
Daniel Toledo <dan992965@gmail.com>
2023-06-14 00:14:51 -0300

    feature: adiciona api de login

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\TransactionController;
 use Illuminate\Support\Facades\Route;
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::get('/users', [UserController::class, 'index']);
 Route::get('/users/{id}', [UserController::class, 'showUser']);
