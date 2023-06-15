28f6ec81e2a474767e95bb736242c7a92ab8706c
root <root@DESKTOP-127AGMC.localdomain>
2023-06-14 23:56:10 -0300

    adiciona api de goals

--- backend/.docker/mysql/dbdata/sys/x@0024session.frm
+++ backend/.docker/mysql/dbdata/sys/x@0024session.frm
@@ -7,7 +7,7 @@ definer_user=mysql.sys
 definer_host=localhost
 suid=0
 with_check_option=0
-timestamp=2023-06-07 18:41:13
+timestamp=2023-06-14 20:37:53
 create-version=1
 source=SELECT * FROM sys.x$processlist WHERE conn_id IS NOT NULL AND command != \'Daemon\'
 client_cs_name=utf8
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\CardController;
+use App\Http\Controllers\Api\GoalController;
 use App\Http\Controllers\Api\TransactionController;
 
 
@@ -24,15 +25,22 @@ Route::patch('/transactions/{id}', [TransactionController::class, 'updateTransac
 Route::delete('/transactions/{id}', [TransactionController::class, 'deleteTransaction']);
 
 # Rotas para o CRUD de cartões
-Route::post('/cards', [CardController::class, 'createCard']);
 Route::get('/cards', [CardController::class, 'index']);
 Route::get('/cards/{id}', [CardController::class, 'showCard']);
+Route::post('/cards', [CardController::class, 'createCard']);
 Route::patch('/cards/{id}', [CardController::class, 'updateCard']);
 Route::delete('/cards/{id}', [CardController::class, 'deleteCard']);
+
+# Rotas para o CRUD de goals
+Route::get('/goals', [GoalController::class, 'index']);
+Route::get('/goals/{id}', [GoalController::class, 'showGoal']);
+Route::post('/goals', [GoalController::class, 'createGoal']);
+Route::patch('/goals/{id}', [GoalController::class, 'updateGoal']);
+Route::delete('/goals/{id}', [GoalController::class, 'deleteGoal']);
 
 # Rota para a página inicial da API
 Route::get('/', function () {
     return response()->json([
-        'message' => 'Rota não autorizada',
+        'message' => 'Erro na rota',
     ]);
 });
