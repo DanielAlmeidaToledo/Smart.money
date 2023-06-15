5b04689be4989b933f7ac6ed3bba06bd4e5ae13b
root <root@DESKTOP-127AGMC.localdomain>
2023-06-15 17:50:56 -0300

    atualizando authcontroller com cookies

--- backend/app/Http/Controllers/Api/AuthController.php
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
-use App\Http\Requests\AuthRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\AuthRequest;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Cookie;
 use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
@@ -14,38 +15,42 @@ class AuthController extends Controller
     public function auth(AuthRequest $request)
     {
         $user = User::where('email', $request->email)->first();
+
         if (!$user || !Hash::check($request->password, $user->password)) {
             throw ValidationException::withMessages([
                 'error' => 'Credenciais inválidas',
             ]);
         }
 
-        return $user;
+        // Define o nome do cookie
+        $cookieName = 'last_activity';
 
-        // $user->tokens()->delete();
+        // Define o tempo de inatividade desejado (1 minuto)
+        $tempo_inatividade = 5;
 
-        // $token = $user->createToken('login')->plainTextToken;
+        // Verifica se o cookie existe e se o usuário está inativo há mais de 1 minuto
+        if ($request->hasCookie($cookieName) && (time() - $request->cookie($cookieName) > $tempo_inatividade)) {
+            // Redireciona o usuário para a página de login
+            return redirect('login')->cookie(Cookie::forget($cookieName));
+        }
 
-        // return response()->json([
-        //     'token' => $token,
-        // ]);
+        // Define o valor do cookie como o tempo atual
+        $cookieValue = time();
+
+        // Retorna a resposta JSON com o usuário autenticado e define o cookie
+        return response()->json([
+            'user' => $user,
+        ])->withCookie($cookieName, $cookieValue, $tempo_inatividade);
     }
+
 
     public function logout(Request $request)
     {
         $request->user()->tokens()->delete();
 
-        return response()->json([
-            'message' => 'success',
-        ]);
-    }
+        // Encerra a sessão e remove as informações de autenticação
+        $request->session()->forget('usuario');
 
-    public function me(Request $request)
-    {
-        $user = $request->user();
-
-        return response()->json([
-            'me' => $user,
-        ]);
+        return redirect('login')->cookie(Cookie::forget('last_activity'));
     }
 }
