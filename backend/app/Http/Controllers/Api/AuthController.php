cab24e613a77ed2ab355d7d59148470a992fd3bb
Daniel Toledo <dan992965@gmail.com>
2023-06-15 11:37:09 -0300

    fix: ajusta erro de login na tela

--- backend/app/Http/Controllers/Api/AuthController.php
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -16,7 +16,7 @@ class AuthController extends Controller
         $user = User::where('email', $request->email)->first();
         if (!$user || !Hash::check($request->password, $user->password)) {
             throw ValidationException::withMessages([
-                'error' => 'Credenciais inválidas',
+                'error' => 'Email ou senha incorretos',
             ]);
         }
 
