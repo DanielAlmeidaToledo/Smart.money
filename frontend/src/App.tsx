cd4a268a4389767cc0f5ef6d97bfdb7386e2a3df
Daniel Toledo <dan992965@gmail.com>
2023-06-17 23:38:12 -0300

    feature: verifica se usuario esta logado nas rotas internas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,30 @@
-import Sidebar from './components/Sidebar/Sidebar';
 import Header from './components/Header/Header';
+import Sidebar from './components/Sidebar/Sidebar';
 
 import './styles/main.scss';
 
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import Cookies from 'universal-cookie';
+import useAuthContext from './contexts/AuthContext';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 function App() {
+  const { user, setUser } = useAuthContext();
+  const navigate = useNavigate();
+  const cookies = new Cookies();
+
+  useEffect(() => {
+    if (!cookies.get('user')) {
+      navigate('/');
+      setUser({
+        id: '',
+        name: '',
+        email: '',
+        created_at: ''
+      });
+    }
+  }, [user, navigate]);
+
   return (
     <div className="__content-and-sidebar-wrapper">
       <Sidebar />
