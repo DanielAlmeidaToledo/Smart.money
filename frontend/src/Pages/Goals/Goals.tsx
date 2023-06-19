d93d77103da108cec90b0795355ec03943145cd5
Daniel Toledo <dan992965@gmail.com>
2023-06-19 18:50:29 -0300

    feature: realiza o get no goals para preencher tabela

--- frontend/src/Pages/Goals/Goals.tsx
+++ frontend/src/Pages/Goals/Goals.tsx
@@ -1,4 +1,7 @@
+import { useState, useEffect } from 'react';
 import cn from 'classnames';
+import axios from '../../api/axios';
+import useAuthContext from '../../contexts/AuthContext';
 
 import './Goals.scss';
 
@@ -13,45 +16,36 @@ import CarIcon from '../../assets/goals/car.svg';
 import SaveMoneyIcon from '../../assets/goals/save-money.svg';
 
 const categories = [
-  { id: 1, name: 'viagem', icon: TripIcon },
+  { id: 1, name: 'VIAGEM', icon: TripIcon },
   { id: 2, name: 'compra', icon: TripIcon },
   { id: 3, name: 'casa', icon: HouseIcon },
-  { id: 4, name: 'carro', icon: CarIcon },
-  { id: 4, name: 'economia', icon: SaveMoneyIcon }
-];
-
-const goals = [
-  {
-    id: 1,
-    title: 'Viagem para o Caribe',
-    type: 'viagem',
-    amount: 20000,
-    balance: 14600
-  },
-  {
-    id: 2,
-    title: 'Corte de gastos',
-    type: 'economia',
-    amount: 8000,
-    balance: 2000
-  },
-  {
-    id: 3,
-    title: 'Casa em condomínio',
-    type: 'casa',
-    amount: 1000000,
-    balance: 920000
-  },
-  {
-    id: 4,
-    title: 'Comprar Audi A3',
-    type: 'carro',
-    amount: 100000,
-    balance: 42000
-  }
+  { id: 4, name: 'CARRO', icon: CarIcon },
+  { id: 5, name: 'economia', icon: SaveMoneyIcon },
+  { id: 6, name: 'outros', icon: SaveMoneyIcon }
 ];
 
 const Goals: React.FC<GoalsProps> = ({ className }) => {
+  const [goals, setGoals] = useState([]);
+  const { user } = useAuthContext();
+
+  useEffect(() => {
+    searchGoals();
+  }, [user?.id]);
+
+  // Realiza a busca das metas do usuário
+  const searchGoals = () => {
+    if (user?.id) {
+      axios
+        .get(`/goals/user/${user.id}`)
+        .then((response) => {
+          setGoals(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
+
   return (
     <div className={cn('__goals-container', className)}>
       <h2>Metas</h2>
@@ -61,59 +55,68 @@ const Goals: React.FC<GoalsProps> = ({ className }) => {
             <img src={SearchIcon} alt="Pesquisar" />
             <input type="text" placeholder="Pesquisar" />
           </div>
+          <button className={cn(className, 'button-add-goal', 'Button')}>Adicionar</button>
         </div>
         <div className={cn('__goals-table')}>
-          <table>
-            <thead>
-              <tr>
-                <th>Nome</th>
-                <th>Progresso</th>
-                <th>Saldo</th>
-                <th>Visualizar</th>
-              </tr>
-            </thead>
-            <tbody>
-              {goals.map((goal) => {
-                const { id, title, type, amount, balance } = goal;
-                return (
-                  <tr key={id}>
-                    <td className="td-name">
-                      {categories.map((categoryItem) => {
-                        if (categoryItem.name === type) {
-                          return (
-                            <img
-                              className="icon-table"
-                              key={categoryItem.id}
-                              src={categoryItem.icon}
-                              alt={categoryItem.name}
-                            />
-                          );
-                        }
-                      })}
-                      {title}
-                    </td>
-                    <td>
-                      <div className="td-progress">
-                        <div className="progress-bar">
-                          <div
-                            className="progress-bar-fill"
-                            style={{ width: `${(balance / amount) * 100}%` }}
-                          ></div>
+          {goals.length === 0 ? (
+            <div className={cn('__goals-empty')}>
+              <h3>Nenhuma meta cadastrada :(</h3>
+            </div>
+          ) : (
+            <table>
+              <thead>
+                <tr>
+                  <th>Nome</th>
+                  <th>Progresso</th>
+                  <th>Saldo</th>
+                  <th>Visualizar</th>
+                </tr>
+              </thead>
+              <tbody>
+                {goals.map((goal) => {
+                  const { id, title, type, amount, balance } = goal;
+                  return (
+                    <tr key={id}>
+                      <td className="td-name">
+                        {categories.map((categoryItem) => {
+                          if (categoryItem.name === type) {
+                            return (
+                              <img
+                                className="icon-table"
+                                key={categoryItem.id}
+                                src={categoryItem.icon}
+                                alt={categoryItem.name}
+                              />
+                            );
+                          }
+                        })}
+                        {title}
+                      </td>
+                      <td>
+                        <div className="td-progress">
+                          <div className="progress-bar">
+                            <div
+                              className="progress-bar-fill"
+                              style={{ width: `${(balance / amount) * 100}%` }}
+                            ></div>
+                          </div>
+                          <span className="progress-bar-text">
+                            {((balance / amount) * 100).toFixed(0)}%
+                          </span>
                         </div>
-                        <span className="progress-bar-text">{(balance / amount) * 100}%</span>
-                      </div>
-                    </td>
-                    <td>
-                      <button className="button-add-balance">Adicionar saldo</button>
-                    </td>
-                    <td>
-                      <button className="button-detail">Detalhes</button>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
+                      </td>
+                      <td>
+                        <button className="button-add-balance">Adicionar saldo</button>
+                      </td>
+                      <td>
+                        <button className="button-detail">Detalhes</button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          )}
         </div>
       </div>
     </div>
