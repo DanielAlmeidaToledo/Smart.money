d4c6711007b7c345826f44f6f8fd52b6fb5a4e9a
Rafael Cerqueira <rafahcerqueira@gmail.com>
2023-06-20 14:57:28 -0300

    adiciona pagina em construcao temporariamente

--- frontend/src/Pages/CardsList/CardsList.tsx
+++ frontend/src/Pages/CardsList/CardsList.tsx
@@ -1,5 +1,5 @@
+import React from 'react';
 import cn from 'classnames';
-
 import './CardsList.scss';
 
 type CardsListProps = {
@@ -7,7 +7,16 @@ type CardsListProps = {
 };
 
 const CardsList: React.FC<CardsListProps> = ({ className }) => {
-  return <div className={cn('__cardslist-container', className)}></div>;
+  return (
+    <div className={cn('__cardslist-container', className)}>
+      <div className={cn('__development-page', 'bg')}>
+        <h3 className="__title">Página em Desenvolvimento</h3>
+        <div className="__clock-container">
+          <div className="loader"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CardsList;
